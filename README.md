b6b5d5bf42ca4ee133ab7a9d2065352497daedf2
narumincho <16481886+narumincho@users.noreply.github.com>
2023-12-13 20:25:24 +0900

    add build_npm.ts

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # simple_graphql_server_common
 
 GraphQL server common library for https://github.com/narumincho/dart-packages/tree/main/packages/simple_graphql_client_gen
+
+## build for npm
+
+```sh
+deno run -A ./build_npm.ts
+```
---
+++ build_npm.ts
@@ -0,0 +1,33 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.39.0/mod.ts";
+import { resolve } from "https://deno.land/std@0.182.0/path/mod.ts";
+
+const npmOutDir = "./npm";
+
+await emptyDir(npmOutDir);
+
+Promise.all([
+  build({
+    entryPoints: ["./annotation.ts", "./dateTime.ts", "./url.ts"],
+    outDir: npmOutDir,
+    shims: {
+      deno: true,
+    },
+    package: {
+      name: "simple-graphql-server-common",
+      version: "0.0.1",
+      author: "narumincho",
+      description:
+        "GraphQL server common library for simple_graphql_client_gen",
+      license: "MIT",
+      repository: {
+        type: "git",
+        url: "git+https://github.com/narumincho/simple_graphql_server_common",
+      },
+      bugs: {
+        url: "https://github.com/narumincho/simple_graphql_server_common/issues",
+      },
+    },
+  }),
+  Deno.copyFile("LICENSE", resolve(npmOutDir, "LICENSE")),
+  Deno.copyFile("README.md", resolve(npmOutDir, "README.md")),
+]);
